682312769eff006411dbed12bb846d718063d198
zhengchangjun <zhengchangjun@jd.com>
2018-01-24 18:36:58 +0800

    maka

--- src/api/copyMaka.js
+++ src/api/copyMaka.js
@@ -1,34 +1,24 @@
+var Maka = require('./../maka');
+var MakaUser = require('./../user/MakaUser');
+var makaSevice = require('./../maka/service');
 var config = require('./../config');
+var makaConfig = config.maka;
+const url = require('url');
 
-var eqxConfig = config.eqx;
-var Scene = require('./../scene');
-var sceneService = require('./../scene/services');
-var EqxUser = require('./../user/eqxUser');
+function copyMaka(target, toAccount, pwd) {
+	let pathname = url.parse(target).pathname;
+	let targetUrl = 'http://viewer.maka.im/k/' + pathname.split('/')[3] + '?mode=storeTemplate&TempAdmode=true';
+	var user = new MakaUser(toAccount, pwd);
+	var maka = new Maka(targetUrl);
+	return maka.loadData().then(res=>user.login().then(loginSuccess));
 
-function copyEqx(url) {
-	var eqxUser = new EqxUser(eqxConfig.eqxName, eqxConfig.eqxPwd);
-	var oriScene = new Scene(url);
-	return oriScene.loadData().then(res=>eqxUser.login().then(loginSuccess));
-
-	function loginSuccess() {
-		sceneService.setHeaders({Origin: eqxConfig.eqxOrigin, cookie: eqxUser.cookie});
-		return sceneService.createScene().then(res=> {
-			var sceneId = JSON.parse(res).obj;
-			return sceneService.getSceneDetail(sceneId).then(res1 => {
-				var json = JSON.parse(res1).obj;
-				var originData = oriScene.data;
-				json.bgAudio = JSON.stringify(originData.bgAudio);
-				json.property = JSON.stringify(originData.property);
-				json.name = originData.name;
-				json.cover = originData.cover;
-				json.description = originData.description;
-				json.pageMode = originData.pageMode;
-				var scene = new Scene(json);
-				scene.user = eqxUser;
-			 	return scene.copy(oriScene.pages);
-			});
+	function loginSuccess(res) {
+		makaService.setHeaders({
+			Origin: makaConfig.origin, 
+			cookie: user.cookie
 		});
+		// return makaService.createTemplate();
 	}
 }
 
-module.exports = copyEqx;
+module.exports = copyMaka;
